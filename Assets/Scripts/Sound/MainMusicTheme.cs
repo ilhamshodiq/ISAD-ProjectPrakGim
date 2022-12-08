a948a7bf5c0ee22bb69dd073e715c14d331d1fbc
rokhimnur27 <kemplimgjr27@gmail.com>
2022-12-09 05:10:11 +0700

    add main sound theme

---
+++ Assets/Scripts/Sound/MainMusicTheme.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMusicTheme : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "Stage 1")
+        {
+            Destroy(gameObject);
+            // MainMusicTheme.bgMusic.GetComponent<AudioSource>().Pause();
+        }
+    }
+
+    // Play music when scene changes
+    private static MainMusicTheme bgMusic;
+
+
+    private void Awake()
+    {
+        if(bgMusic == null)
+        {
+            bgMusic = this;
+            DontDestroyOnLoad(bgMusic);
+        }
+
+        else 
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
